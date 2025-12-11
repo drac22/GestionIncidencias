14d8fca44bd851ec8530d693c5369bbf36b94e8d
drac22 <aronecalle22@gmail.com>
2025-12-11 08:45:05 -0500

    chore: initial Spring Boot

---
+++ src/main/java/com/personalproject/GestionIncidencias/GestionIncidenciasApplication.java
@@ -0,0 +1,13 @@
+package com.personalproject.GestionIncidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionIncidenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionIncidenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalproject/GestionIncidencias/GestionIncidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproject.GestionIncidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionIncidenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
